1f1443b4972e0784bc9054929c4aa1e1f896a028
vladimir-rusaleev <vladimir@GIGABYTE-G5.>
2025-05-03 18:31:47 +0300

    2

--- kernel/dev.c
+++ kernel/dev.c
@@ -18,7 +18,8 @@ struct nullstat_data {
     uint64 count;
     struct spinlock lock;
 } nullstat;
-  
+
+struct spinlock rand_lock;
 uint64 rand_seed = 1;
   
 static uint64 lcg(uint seed) {
@@ -31,17 +32,23 @@ int dev_read(short minor, int user_dst, uint64 dst, int n) {
         case DEV_NULL: return 0;
 
         case DEV_ZERO:
-            char zero_byte = 0;
-            for (int i = 0; i < n; ++i) {
-                if (either_copyout(user_dst, dst + i, &zero_byte, 1) < 0)
+            char zeros[128] = {0};
+            int copied = 0;
+            
+            while (copied < n) {
+                int to_copy = n - copied < sizeof(zeros) ? n-copied : sizeof(zeros);
+                if (either_copyout(user_dst, dst + copied, zeros, to_copy) < 0)
                     return -1;
+                copied += to_copy;
             }
             return n;
         
         case DEV_URANDOM:
             for (int i = 0; i < n; ++i) {
+                acquire(&rand_lock);
                 rand_seed = lcg(rand_seed);
                 char urand = rand_seed & 0xFF;
+                release(&rand_lock);
 
                 if (either_copyout(user_dst, dst + i, &urand, 1) < 0)
                     return -1;
@@ -82,11 +89,13 @@ int dev_write(short minor, int user_src, uint64 src, int n) {
             uint64 new_seed;
             if (either_copyin(&new_seed, user_src, src, n) < 0)
                 return -1;
-
+            
+            acquire(&rand_lock);
             if(new_seed)
                 rand_seed = new_seed;
             else
                 rand_seed = 1;
+            release(&rand_lock);
 
             return n;
         
@@ -104,6 +113,7 @@ int dev_write(short minor, int user_src, uint64 src, int n) {
 
 void dev_init(void) {
     initlock(&nullstat.lock, "dev");
+    initlock(&rand_lock, "rand");
     devsw[DEV].read = dev_read;
     devsw[DEV].write = dev_write;
   }
